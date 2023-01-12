6ed7c70c626139f8938b7a5d6781bd92585bada8
OliverShenton <84964225+OliverShenton@users.noreply.github.com>
2023-01-12 23:31:12 +0000

    Add files via upload

---
+++ 40.Polymorphism/src/Ford.java
@@ -0,0 +1,21 @@
+public class Ford extends Car {
+
+    public Ford(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Ford " + super.startEngine();
+    }
+
+    @Override
+    public String accelerate() {
+        return "Ford " + super.accelerate();
+    }
+
+    @Override
+    public String brake() {
+        return "Ford " + super.brake();
+    }
+}
