99669f9e372988937564c4f256f0a63e75308053
OliverShenton <84964225+OliverShenton@users.noreply.github.com>
2023-01-12 20:18:34 +0000

    Add files via upload

---
+++ 13.NumberInWord/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        NumberInWord.printNumberInWord(2);
+        NumberInWord.printNumberInWord(4);
+        NumberInWord.printNumberInWord(6);
+        NumberInWord.printNumberInWord(-2);
+        NumberInWord.printNumberInWord(10);
+
+    }
+
+}
