f0f13d116f3c743bca331d653977016094b0e942
OliverShenton <84964225+OliverShenton@users.noreply.github.com>
2023-01-12 20:43:04 +0000

    Delete BarkingDog directory

--- BarkingDog/src/BarkingDog.java
+++
@@ -1,9 +0,0 @@
-public class BarkingDog {
-
-    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
-        if (hourOfDay > 23 || hourOfDay < 0) {
-            return false;
-        } else return barking && hourOfDay < 8 || barking && hourOfDay > 22;
-    }
-
-}
