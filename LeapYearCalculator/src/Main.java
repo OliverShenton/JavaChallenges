648513f2150cecbb8e5d63ebaa618a3addb8ddb3
OliverShenton <84964225+OliverShenton@users.noreply.github.com>
2023-01-12 16:21:13 +0000

    Add files via upload

---
+++ LeapYearCalculator/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        LeapYear.isLeapYear(-1600);
+        LeapYear.isLeapYear(1600);
+        LeapYear.isLeapYear(2017);
+        LeapYear.isLeapYear(2000);
+
+    }
+
+}
